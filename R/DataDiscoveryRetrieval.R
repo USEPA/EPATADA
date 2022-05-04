907a38a56e2d633eabfe3a4a37bcd062814543df
Cristina Mullin <mullin.cristina@epa.gov>
2022-05-04 19:33:45 -0400

    Added vignettes and data retrieval functions

--- R/DataDiscoveryRetrieval.R
+++ R/DataDiscoveryRetrieval.R
@@ -61,29 +61,81 @@ TADAprofileCheck <- function(.data){
 #' Read in WQP data using WQP web services directly 
 #' 
 #' Go to the WQP website (https://www.waterqualitydata.us/) and fill out the 
-#' advanced query form. When finished, do not download the data. Then choose
-#' the Full Physical Chemical Data Profile, all data sources, and the file 
-#' format Comma-Separated. Do hit the download button. Instead, copy the 
-#' web service URL located at the bottom of the page (under result). Use that 
-#' Result web service URL as the input for this function to download data 
-#' directly into R.  
+#' advanced query form. Choose the Full Physical Chemical Data Profile, all data
+#' sources, and the file format Comma-Separated. When finished, do not hit the 
+#' download button. Instead, copy the web service URL located at the bottom of 
+#' the page under the header "Result". Use that "Result" web service URL as the
+#' input for this function to download data directly into R.  
 #' 
 #' Note: It may be useful to save the Query URL as well as a comment within 
 #' your code. This URL let's you return to the WQP query page with all your 
 #' original data filters.
-#'
+#' 
+#' @param webservice WQP Web Service URL
 #' @return WQP Full Physical Chemical Results Data Profile 
 #' @export
 #' 
 
-readWQPWebService <- function(WebService) {
+readWQPwebservice <- function(webservice) {
   #read in csv from WQP web service
-  if(grepl("zip=yes", WebService)){
-    WebService=str_replace(WebService, "zip=yes", "zip=no")
-    data.table::fread(toString(WebService))
+  if(grepl("zip=yes", webservice)){
+    webservice=stringr::str_replace(webservice, "zip=yes", "zip=no")
+    return (data.table::fread(toString(webservice)))
     } else {
-    data.table::fread(WebService)
+    return (data.table::fread(webservice))
   }
 }
 
+#' autoclean 
+#' 
+#' Removes complex biological data. Removes non-water media samples. 
+#' Removes rows of data that are true duplicates. Capitalizes fields to harmonize
+#' data.
+#' 
+#' Within "BiologicalIntentName", only the allowable values "tissue", "toxicity", 
+#' and "NA" apply to non-biological data (the function removes all others).
+#' Toxicity and fish tissue data will be kept, but other types of biological 
+#' monitoring data will not.
+#' 
+#' We decided to make some fields uppercase that way they're more compatible 
+#' with the WQX validation reference tables and to avoid any issues with 
+#' case-sensitivity when joining data. Therefore, we might need to tack on any
+#' immediate QA steps (removing true duplicates, converting result values to numeric,
+#' capitalizing letters, etc.) to this function, as well as the other retrieval functions.
+#' 
+#' @param .data TADA data profile
+#' @return autocleaned TADA data profile 
+#' @export
+#' 
 
+autoclean <- function(.data, clean=TRUE){
+  # check that .data object is compatible with TADA
+  # check .data is of class data.frame
+  if("data.frame" %in% class(.data) == FALSE) {
+    stop("Input object must be of class 'data.frame'")
+  }
+  # check .data has required columns
+  if(all(c("ActivityMediaName", "ResultMeasure.MeasureUnitCode",
+           "CharacteristicName", "ResultSampleFractionText", "MethodSpecificationName") %in% colnames(.data)) == FALSE) {
+    stop("The dataframe does not contain the required fields to use this TADA function. Use either the full physical/chemical profile downloaded from WQP or download the TADA profile template available on the EPA TADA webpage.")
+  }
+  # execute function after checks are passed
+  if(all(c("ActivityMediaName", "ResultMeasure.MeasureUnitCode",
+           "CharacteristicName", "ResultSampleFractionText", "MethodSpecificationName") %in% colnames(.data)) == TRUE)
+    # capitalize fields (just those used w/ ref tables for now)
+    .data$CharacteristicName = toupper(.data$CharacteristicName)
+    .data$ResultSampleFractionText = toupper(.data$ResultSampleFractionText)
+    .data$MethodSpecificationName = toupper(.data$MethodSpecificationName)
+    .data$ResultMeasure.MeasureUnitCode = toupper(.data$ResultMeasure.MeasureUnitCode)
+    .data$ActivityMediaName = toupper(.data$ActivityMediaName)
+    #.data$BiologicalIntentName = toupper(.data$BiologicalIntentName)
+    
+    # Remove duplicate rows
+    .data = .data[!duplicated(.data),]
+    
+    # Remove complex biological data
+    .data = dplyr::filter(.data, ActivityMediaName == "WATER")
+    #.data = dplyr::filter(.data, BiologicalIntentName != "TISSUE" | "TOXICITY" | is.na(InvalidCoordinates)== TRUE)
+    
+    return (.data)
+}
