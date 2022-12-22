b96eb6600accebc8ba92364f2152af30d36ce08b
Katie Healy <healy.kathleen@epa.gov>
2022-12-22 11:08:56 -0600

    Update ResultFlagsDependent.R
    Added an "errorsonly" parameter to InvalidFraction() which filters data to show only the invalid characteristic-fraction combinations when errorsonly = TRUE.

--- R/ResultFlagsDependent.R
+++ R/ResultFlagsDependent.R
@@ -2,17 +2,25 @@
 #'
 #' Function checks the validity of each characteristic-fraction combination
 #' in the dataframe. When clean = TRUE, rows with invalid characteristic-fraction
-#' combinations are removed. Default is clean = TRUE.
+#' combinations are removed. Default is clean = TRUE. When errorsonly = TRUE, only
+#' invalid characteristic-fraction combinations are returned. Default is errorsonly = FALSE.
 #'
 #' @param .data TADA dataframe
 #' @param clean Boolean argument; removes "Invalid" characteristic-fraction
 #' combinations from the dataframe when clean = TRUE. Default is clean = TRUE.
+#' @param errorsonly Boolean argument; filters to show only the "Invalid"
+#' characteristic-fraction combinations in the dataframe when errorsonly = TRUE.
+#' Default is errorsonly = FALSE.
 #'
-#' @return When clean = FALSE, this function adds the following column to your
-#' dataframe: WQX.SampleFractionValidity. This column flags each 
+#' @return When clean = FALSE and errorsonly = FALSE, this function adds the
+#' following column to your dataframe: WQX.SampleFractionValidity. This column flags each 
 #' CharacteristicName and ResultSampleFractionText combination in your dataframe 
-#' as either "Nonstandardized", "Invalid", or "Valid". When clean = TRUE, 
-#' "Invalid" rows are removed from the dataframe and no column will be appended.
+#' as either "Nonstandardized", "Invalid", or "Valid". When clean = FALSE and 
+#' errorsonly = TRUE, the WQX.SampleFractionValidity is still added and the data
+#' is filtered to only show the "Invalid" rows only. When clean = TRUE and
+#' errorsonly = FALSE, "Invalid" rows are removed from the dataframe and no 
+#' column will be appended. When clean = TRUE and errorsonly = TRUE, the function
+#' does not execute and an error message is returned.
 #'
 #' @export
 #' 
@@ -26,17 +34,25 @@
 #' # Flag, but do not remove, data with invalid characteristic-fraction combinations
 #' # in new column titled "WQX.SampleFractionValidity":
 #' InvalidFraction_flags <- InvalidFraction(Nutrients_Utah, clean = FALSE)
-#'
+#' 
+#' # Show only invalid characteristic-fraction combinations:
+#' InvalidFraction_errorsonly <- InvalidFraction(Nutrients_Utah, clean = FALSE, errorsonly = TRUE)
 
 
-InvalidFraction <- function(.data, clean = TRUE) {
+InvalidFraction <- function(.data, clean = TRUE, errorsonly = FALSE) {
   # check .data is data.frame
   checkType(.data, "data.frame", "Input object")
   # check clean is boolean
   checkType(clean, "logical")
+  # check errorsonly is boolean
+  checkType(errorsonly, "logical")
   # check .data has required columns
   checkColumns(.data, c("CharacteristicName", "ResultSampleFractionText"))
-
+  # check that both clean and errorsonly are not TRUE
+  if (clean == TRUE & errorsonly == TRUE) {
+    stop("Function not executed because clean and errorsonly cannot both be TRUE")
+  }
+  
   # execute function after checks are passed
   if (("WQX.SampleFractionValidity" %in% colnames(.data)) == TRUE) {
     .data <- dplyr::select(.data, -WQX.SampleFractionValidity)
@@ -80,14 +96,14 @@ InvalidFraction <- function(.data, clean = TRUE) {
     return(.data)
   }
   
-  # flagged output
-  if (clean == FALSE) {
+  # flagged output, all data
+  if (clean == FALSE & errorsonly == FALSE) {
     return(check.data)
     warning("Metadata transformations may be adversely affected by choosing to retain 'Invalid' fraction. In order to ensure transformation functions will run properly, set clean = TRUE.")
   }
   
   # clean output
-  if (clean == TRUE) {
+  if (clean == TRUE & errorsonly == FALSE) {
     # filter out invalid characteristic-fraction combinations
     clean.data <- dplyr::filter(check.data, WQX.SampleFractionValidity != "Invalid")
     
@@ -95,6 +111,13 @@ InvalidFraction <- function(.data, clean = TRUE) {
     clean.data <- dplyr::select(clean.data, -WQX.SampleFractionValidity)
     
     return(clean.data)
+  }
+  
+  # flagged output, errors only
+  if (clean == FALSE & errorsonly == TRUE) {
+    # filter out valid characteristic-fraction combinations
+    invalid.data <- dplyr::filter(check.data, WQX.SampleFractionValidity == "Invalid")
+    return(invalid.data)
   }
 }
 
