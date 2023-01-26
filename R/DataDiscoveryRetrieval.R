0a1c1f61ad98ad437a32c7afc37cc30f5da09594
cristinamullin <mullin.cristina@epa.gov>
2023-01-26 17:00:56 -0500

    TADAReadWQPWebServices
    updated name and documentation

--- R/DataDiscoveryRetrieval.R
+++ R/DataDiscoveryRetrieval.R
@@ -141,27 +141,49 @@ TADAdataRetrieval <- function(statecode = "null",
 
 
 
-#' Read in WQP data using WQP web services directly
+#' Read in WQP data using the Water Quality Portal (WQP) web services
 #'
 #' Go to the WQP website (https://www.waterqualitydata.us/) and fill out the
-#' advanced query form. Choose the Full Physical Chemical Data Profile, all data
-#' sources, and the file format Comma-Separated. When finished, do not hit the
-#' download button. Instead, copy the web service URL located at the bottom of
-#' the page under the header "Result". Use that "Result" web service URL as the
-#' input for this function to download data directly into R.
+#' advanced query form. Choose the file format Comma-Separated. 
+#' Then, choose a data profile. When finished, do not hit the download button.
+#' Instead, copy the web service URL located at the bottom of the page under 
+#' the header "Station" or "Result". This is the url in the second box from the 
+#' top. Use that web service URL as the input for this function to download 
+#' data directly into R.
+#' 
+#' We recommend retrieving data for all the following profiles 
+#' (you can run this function four separate times to bring in all four profiles):
+#' 1. Sample Results (physical/chemical metadata)
+#' 2. Sample Results (narrow)
+#' 3. Project Data
+#' 4. Site Data Only
+#' 
+#' After you retrieve all four profiles, you can use TADA::JoinWQPProfiles to 
+#' joining the four dataframes into a single dataframe.
+#' 
+#' Note: It may be useful to save the Query URL from the WQP as well as a 
+#' comment within your code. This URL let's you return to the WQP query page 
+#' with all your selected data filters. For example, this is the query used
+#' in the examples for this function: 
+#' https://www.waterqualitydata.us/#statecode=US%3A09&sampleMedia=water&sampleMedia=Water&startDateLo=01-01-2021&mimeType=csv&dataProfile=biological&providers=NWIS&providers=STEWARDS&providers=STORET
 #'
-#' Note: It may be useful to save the Query URL as well as a comment within
-#' your code. This URL let's you return to the WQP query page with all your
-#' original data filters.
+#' @param webservice WQP Web Service URL, entered within quotes, i.e., "webserviceurl"
 #'
-#' @param webservice WQP Web Service URL, entered within quotes "url"
-#'
-#' @return WQP Full Physical Chemical Results Data Profile
+#' @return WQP Data Profile
 #'
 #' @export
+#' 
+#' @examples 
+#' \dontrun{
+#' physchemresults1 <- TADAReadWQPWebServices("https://www.waterqualitydata.us/data/Result/search?statecode=US%3A09&sampleMedia=water&sampleMedia=Water&startDateLo=01-01-2021&mimeType=csv&zip=yes&dataProfile=biological&providers=NWIS&providers=STEWARDS&providers=STORET")
+#' sites1 <- TADAReadWQPWebServices("https://www.waterqualitydata.us/data/Station/search?statecode=US%3A09&sampleMedia=water&sampleMedia=Water&startDateLo=01-01-2021&mimeType=csv&zip=yes&providers=NWIS&providers=STEWARDS&providers=STORET")
+#' projects1 <- TADAReadWQPWebServices("https://www.waterqualitydata.us/data/Project/search?statecode=US%3A09&sampleMedia=water&sampleMedia=Water&startDateLo=01-01-2021&mimeType=csv&zip=yes&providers=NWIS&providers=STEWARDS&providers=STORET")
+#' narrow1 <- TADAReadWQPWebServices("https://www.waterqualitydata.us/data/Result/search?statecode=US%3A09&sampleMedia=water&sampleMedia=Water&startDateLo=01-01-2021&mimeType=csv&zip=yes&dataProfile=narrowResult&providers=NWIS&providers=STEWARDS&providers=STORET")
+#' }
+#'
 #'
 
-readWQPwebservice <- function(webservice) {
+TADAReadWQPWebServices <- function(webservice) {
   #consider function dataRetrieval::getWebServiceData
   # read in csv from WQP web service
   if (grepl("zip=yes", webservice)) {
