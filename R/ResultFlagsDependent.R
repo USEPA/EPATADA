52d9266069765f5a244a91c1cbca93092bf7369c
Katie Healy <healy.kathleen@epa.gov>
2023-02-06 17:46:50 -0600

    Invalid Speciation update
    Added more options for clean argument in Invalid Speciation function - instead of clean = TRUE/FALSE, clean = c("invalid_only", "nonstandardized_only", "both", "none") which gives the user more flexibility to decide which rows to remove/retain.

--- R/ResultFlagsDependent.R
+++ R/ResultFlagsDependent.R
@@ -133,29 +133,36 @@ InvalidFraction <- function(.data, clean = TRUE, errorsonly = FALSE) {
 #' Check Method Speciation Validity
 #'
 #' Function checks the validity of each characteristic-method
-#' speciation combination in the dataframe. When clean = TRUE, rows with invalid
+#' speciation combination in the dataframe. When clean = "invalid_only", rows with invalid
 #' characteristic-method speciation combinations are removed. Default is
-#' clean = TRUE. When errorsonly = TRUE, dataframe is filtered to show only
-#' rows with invalid characteristic-method speciation combinations. Default is
-#' errorsonly = FALSE.
+#' clean = "invalid_only". When errorsonly = TRUE, dataframe is filtered to show only
+#' rows with invalid or nonstandardized characteristic-method speciation combinations. 
+#' Default is errorsonly = FALSE.
 #'
 #' @param .data TADA dataframe
-#' @param clean Boolean argument; removes "Invalid" characteristic-method
-#' speciation combinations from the dataframe when clean = TRUE. Default is
-#' clean = TRUE.
+#' @param clean Character argument with options "invalid_only", "nonstandardized_only", 
+#' "both", or "none." The default is clean = "invalid_only" which removes rows of
+#' data flagged as having "Invalid" characteristic-method speciation combinations. When
+#' clean = "nonstandardized_only", the function removes rows of data flagged as
+#' having "Nonstandardized" characteristic-method speciation combinations. When 
+#' clean = "both", the function removes rows of data flagged as either "Invalid" or
+#' "Nonstandardized". And when clean = "none", the function does not remove any "Invalid"
+#' or "Nonstandardized" rows of data.
 #' @param errorsonly Boolean argument; filters to show only the "Invalid" 
 #' characteristic-method speciation combinations from the dataframe when 
 #' errorsonly = TRUE. Default is errorsonly = FALSE.
 #'
-#' @return When clean = FALSE and errorsonly = FALSE, this function adds the 
+#' @return When clean = "none" and errorsonly = FALSE, this function adds the 
 #' following column to your dataframe: WQX.MethodSpeciationValidity. This column 
 #' flags each CharacteristicName and MethodSpecificationName combination in your 
-#' dataframe as either "Nonstandardized", "Invalid", or "Valid". When clean = FALSE 
-#' and errorsonly = TRUE, the dataframe is filtered to show only the "Invalid" data; 
-#' the column WQX.MethodSpeciationValidity is still appended. When clean = TRUE
-#' and errorsonly = FALSE, "Invalid" rows are removed from the dataframe and 
-#' no column will be appended. When clean = TRUE and errorsonly = TRUE, the
-#' function is not executed and an error message is returned.
+#' dataframe as either "Nonstandardized", "Invalid", or "Valid". When clean = "none" 
+#' and errorsonly = TRUE, the dataframe is filtered to show only the "Invalid" and
+#' "Nonstandardized data; the column WQX.MethodSpeciationValidity is still appended. 
+#' When clean = "invalid_only" and errorsonly = FALSE, "Invalid" rows are removed 
+#' from the dataframe, but "Nonstandardized" rows are retained. When
+#' clean = "nonstandardized_only" and errorsonly = FALSE, "Nonstandardized" rows
+#' are removed, but "Invalid" rows are retained. The default is clean = "invalid_only"
+#' and errorsonly = FALSE.
 #'
 #' @export
 #'
@@ -163,30 +170,40 @@ InvalidFraction <- function(.data, clean = TRUE, errorsonly = FALSE) {
 #' # Load example dataset:
 #' data(Nutrients_Utah)
 #' 
-#' # Remove data with invalid characteristic-method speciation combinations from dataframe:
+#' # Remove data with invalid characteristic-method speciation combinations from dataframe, 
+#' # but retain nonstandardized combinations flagged in new column 'WQX.MethodSpeciationValidity':
 #' InvalidSpeciation_clean <- InvalidSpeciation(Nutrients_Utah)
 #' 
-#' # Flag, but do not remove, data with invalid characteristic-method speciation
+#' # Remove data with nonstandardized characteristic-method speciation combinations 
+#' # from dataframe but retain invalid combinations flagged in new column 'WQX.MethodSpeciationValidity':
+#' NonstandardSpeciation_clean <- InvalidSpeciation(Nutrients_Utah, clean = "nonstandardized_only")
+#' 
+#' # Remove both invalid and nonstandardized characteristic-method speciation combinations
+#' # from dataframe:
+#' Speciation_clean <- InvalidSpeciation(Nutrients_Utah, clean = "both")
+#' 
+#' # Flag, but do not remove, data with invalid or nonstandardized characteristic-method speciation
 #' # combinations in new column titled "WQX.MethodSpeciationValidity":
-#' InvalidSpeciation_flags <- InvalidSpeciation(Nutrients_Utah, clean = FALSE)
+#' InvalidSpeciation_flags <- InvalidSpeciation(Nutrients_Utah, clean = "none")
 #' 
 #' # Show only invalid characteristic-method speciation combinations:
-#' InvalidSpeciation_errorsonly <- InvalidSpeciation(Nutrients_Utah, clean = FALSE, errorsonly = TRUE)
+#' InvalidSpeciation_errorsonly <- InvalidSpeciation(Nutrients_Utah, clean = "nonstandardized_only", errorsonly = TRUE)
+#' 
+#' # Show only nonstandardized characteristic-method speciation combinations:
+#' NonstandardSpeciation_errorsonly <- InvalidSpeciation(Nutrients_Utah, clean = "invalid_only", errorsonly = TRUE)
 
 
-InvalidSpeciation <- function(.data, clean = TRUE, errorsonly = FALSE) {
+InvalidSpeciation <- function(.data, clean = c("invalid_only", "nonstandardized_only", "both", "none"), errorsonly = FALSE) {
   # check .data is data.frame
   checkType(.data, "data.frame", "Input object")
   # check clean is boolean
-  checkType(clean, "logical")
+  checkType(clean, "character")
   # check errorsonly is boolean
   checkType(errorsonly, "logical")
   # check .data has required columns
   checkColumns(.data, c("CharacteristicName", "MethodSpecificationName"))
-  # check that clean and errorsonly are not both TRUE
-  if (clean == TRUE & errorsonly == TRUE) {
-    stop("Function not executed because clean and errorsonly cannot both be TRUE")
-  }
+  # check that clean is either "invalid_only", "nonstandardized_only", "both", or "none"
+  clean <- match.arg(clean)
 
   # execute function after checks are passed
   if (("WQX.MethodSpeciationValidity" %in% colnames(.data)) == TRUE) {
@@ -229,31 +246,49 @@ InvalidSpeciation <- function(.data, clean = TRUE, errorsonly = FALSE) {
   }
   
   # flagged output, all data
-  if (clean == FALSE & errorsonly == FALSE) {
-    return(check.data)
-    warning("Metadata transformations may be adversely affected by choosing to retain 'Invalid' speciation. In order to ensure transformation functions will run properly, set clean = TRUE.")
+  if (clean == "none" & errorsonly == FALSE) {
+    warning("Metadata transformations may be adversely affected by choosing to retain 'Invalid' speciation. In order to ensure transformation functions will run properly, set clean = 'invalid_only' or 'both'.")
   }
   
-  # clean output
-  if (clean == TRUE & errorsonly == FALSE) {
-    # filter out invalid characteristic-method speciation combinations
+  # when clean = "invalid_only"
+  if (clean == "invalid_only") {
+    # filter out only "Invalid" characteristic-method speciation combinations
     clean.data <- dplyr::filter(check.data, WQX.MethodSpeciationValidity != "Invalid")
-    
-    # remove WQX.MethodSpeciationValidity column
-    clean.data <- dplyr::select(clean.data, -WQX.MethodSpeciationValidity)
-    
+  }
+  
+  # when clean = "nonstandardized_only"
+  if (clean == "nonstandardized_only") {
+    # filter out only "Nonstandardized" characteristic-method speciation combinations
+    clean.data <- dplyr::filter(check.data, WQX.MethodSpeciationValidity != "Nonstandardized")
+  }
+  
+  # when clean = "both"
+  if (clean == "both") {
+    # filter out both "Invalid" and "Nonstandardized" characteristic-method speciation combinations
+    clean.data <- dplyr::filter(check.data, WQX.MethodSpeciationValidity != "Nonstandardized" | WQX.MethodSpeciationValidity != "Invalid")
+  }
+  
+  # when clean = "none"
+  if (clean == "none") {
+    # retain all data
+    clean.data <- check.data
+  }
+  
+  # when errorsonly = FALSE
+  if (errorsonly == FALSE) {
     return(clean.data)
   }
   
-  # flagged output, errors only
-  if (clean == FALSE & errorsonly == TRUE) {
-    # filter to show only invalid characteristic-method speciation combinations
-    invalid.data <- dplyr::filter(check.data, WQX.MethodSpeciationValidity == "Invalid")
-    if (nrow(invalid.data) == 0) {
-      print("This dataframe is empty because we did not find any invalid characteristic-method speciation combinations in your dataframe")
-      invalid.data <- dplyr::select(invalid.data, -WQX.MethodSpeciationValidity)
+  # when errorsonly = TRUE
+  if (errorsonly == TRUE) {
+    # filter to show only invalid and/or nonstandardized characteristic-method speciation combinations
+    error.data <- dplyr::filter(clean.data, WQX.MethodSpeciationValidity == "Invalid" | WQX.MethodSpeciationValidity == "Nonstandardized")
+    # if there are no errors
+    if (nrow(error.data) == 0) {
+      print("This dataframe is empty because either we did not find any invalid/nonstandardized characteristic-method speciation combinations or they were all filtered out")
+      error.data <- dplyr::select(error.data, -WQX.MethodSpeciationValidity)
     }
-    return(invalid.data)
+    return(error.data)
   }
 }
 
