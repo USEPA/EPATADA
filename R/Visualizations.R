c0d4f4a64a0d12d80b78334a2fde82638092f605
Elise H <121896266+ehinman@users.noreply.github.com>
2023-02-22 13:45:36 -0500

    visualizations file change and documentation
     made summarizecharacteristics not column-specific. Will summarize any column.

--- R/Visualizations.R
+++ R/Visualizations.R
@@ -1,27 +1,29 @@
-#' @title Summarize data downloaded for each Characteristic
+#' @title Summarize data downloaded by specified column
 #' 
 #' @description 
 #' Function to summarize the number of sites and records downloaded from the 
-#' WQP for each CharacteristicName.
+#' WQP for each unique column group.
 #' 
 #' @param .data TADA data frame containing the data downloaded from the WQP, where
 #' each row represents a unique data record.
+#' @param col A text string name of the column the user would like summarized.
 #' 
-#' @return 
-#' Saves a .csv file containing the total number of sites and records downloaded
-#' from the Water Quality Portal for each requested characteristic name.
+#' @return A dataframe containing a column for each unique element, the number of
+#' sites with that element populated, and the number of records with that element
+#' populated.
 #'  
 #' @export
 #'  
 
-SummarizeCharacteristics <- function(.data){
-  
+SummarizeColumn <- function(.data,col){
+  .data$summ = .data[,col]
   # Summarize WQP data pull
   wqp_summary <- .data %>%
-    dplyr::group_by(CharacteristicName) %>%
+    dplyr::group_by(summ) %>%
     dplyr::summarize(n_sites = length(unique(MonitoringLocationIdentifier)),
-                     n_records = length(ResultMeasureValue),
+                     n_records = length(TADA.ResultMeasureValue),
                      .groups = 'drop') %>%
-    dplyr::select(CharacteristicName, n_sites, n_records)
-  
+    dplyr::select(summ, n_sites, n_records)
+  names(wqp_summary)[names(wqp_summary)=="summ"] = col
+  return(wqp_summary)
 }
