4c9f278caee9a9a41ae9c35420c4b858b057c6bd
cristinamullin <mullin.cristina@epa.gov>
2023-04-05 22:44:52 -0400

    updates

---
+++ vignettes/CONTRIBUTING.R
@@ -0,0 +1,10 @@
+## ----setup, include=FALSE-----------------------------------------------------
+library(knitr)
+
+knitr::opts_chunk$set(echo = TRUE,
+               warning = FALSE,
+               message = FALSE)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  install.packages(c("devtools", "rmarkdown"))
+
