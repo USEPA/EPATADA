b7016af831e665c4fa0c1c43071e4bb2f52aba24
Cristina Mullin <46969696+cristinamullin@users.noreply.github.com>
2024-03-11 14:09:23 -0400

    export pipe

--- R/Utilities.R
+++ R/Utilities.R
@@ -7,6 +7,7 @@
 #' @keywords internal
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
+#' @export
 #' @param lhs A value or the magrittr placeholder.
 #' @param rhs A function call using the magrittr semantics.
 #' @return The result of calling `rhs(lhs)`.
