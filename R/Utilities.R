05127be0b2ad5015ac970d08588f26b1b7ba1abb
hillarymarler <Marler.Hillary@epa.gov>
2024-05-07 11:54:15 -0400

    Update Utilities.R

--- R/Utilities.R
+++ R/Utilities.R
@@ -400,7 +400,7 @@ TADA_ConvertSpecialChars <- function(.data, col, percent.ave = TRUE) {
   chars.data <- .data
   names(chars.data)[names(chars.data) == col] <- "orig"
   chars.data <- chars.data %>%
-    dplyr::select(-any_of(col, numcol, flagcol))
+    dplyr::select(-any_of(c(col, numcol, flagcol)))
   chars.data$masked <- chars.data$orig
 
   # Add percentage character to dissolved oxygen saturation ResultMeasureValue
