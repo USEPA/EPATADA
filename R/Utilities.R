a8b54d7ad5a37af35af79f325c226aea20236897
Renae Myers <renaemyers@gmail.com>
2024-06-26 16:08:43 -0700

    Update TADA_FindPotentialDuplicatesMultipleOrgs to use new column name

--- R/Utilities.R
+++ R/Utilities.R
@@ -724,7 +724,7 @@ TADA_CreateComparableID <- function(.data) {
 #' @param dist_buffer Numeric. The maximum distance (in meters) two sites can be
 #'   from one another to be considered "nearby" and grouped together.
 #'
-#' @return Input dataframe with a TADA.NearbySiteGroups column that indicates
+#' @return Input dataframe with a TADA.MonitoringLocationIdentifier column that indicates
 #'   the nearby site groups each monitoring location belongs to.
 #'
 #' @export
@@ -732,26 +732,26 @@ TADA_CreateComparableID <- function(.data) {
 TADA_FindNearbySites <- function(.data, dist_buffer = 100) {
   # check .data is data.frame
   TADA_CheckType(.data, "data.frame", "Input object")
-
+  
   # .data required columns
   required_cols <- c("MonitoringLocationIdentifier", "TADA.LongitudeMeasure", "TADA.LatitudeMeasure")
   # check .data has required columns
   TADA_CheckColumns(.data, required_cols)
-
+  
   # create spatial dataset based on sites
   data_sf <- unique(.data[, c("MonitoringLocationIdentifier", "TADA.LongitudeMeasure", "TADA.LatitudeMeasure")])
   # convert to sf object
   data_sf <- sf::st_as_sf(data_sf,
-    coords = c("TADA.LongitudeMeasure", "TADA.LatitudeMeasure"),
-    # Change to your CRS
-    crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+                          coords = c("TADA.LongitudeMeasure", "TADA.LatitudeMeasure"),
+                          # Change to your CRS
+                          crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
   )
   # create a distance matrix in meters
   dist.mat <- data.frame(sf::st_distance(data_sf)) # Great Circle distance since in lat/lon
-
+  
   row.names(dist.mat) <- data_sf$MonitoringLocationIdentifier
   colnames(dist.mat) <- data_sf$MonitoringLocationIdentifier
-
+  
   # convert distances to those within buffer (1) and beyond buffer (0)
   dist.mat1 <- apply(dist.mat, c(1, 2), function(x) {
     if (x <= dist_buffer) {
@@ -760,10 +760,10 @@ TADA_FindNearbySites <- function(.data, dist_buffer = 100) {
       x <- 0
     }
   })
-
+  
   # create empty dataframe for groups
   groups <- data.frame()
-
+  
   # loop through distance matrix and extract site groups that are within the buffer distance from one another
   for (i in 1:dim(dist.mat1)[1]) {
     fsite <- rownames(dist.mat1)[i] # focal site
@@ -772,48 +772,50 @@ TADA_FindNearbySites <- function(.data, dist_buffer = 100) {
     sites <- dat$MonitoringLocationIdentifier[dat$Count == 1] # filter to sites within buffer
     sites1 <- sites[!sites %in% fsite] # get site list within buffer that does not include focal site
     if (length(sites1) > 0) { # if this list is greater than 0, combine sites within buffer into data frame
-      df <- data.frame(MonitoringLocationIdentifier = sites, TADA.SiteGroup = paste0(sites, collapse = ", "))
+      df <- data.frame(MonitoringLocationIdentifier = sites, TADA.MonitoringLocationIdentifier = paste0(sites, collapse = ","))
+      df[c("TADA.MonitoringLocationIdentifier")] <- lapply(df[c("TADA.MonitoringLocationIdentifier")], TADA_FormatDelimitedString)
       groups <- plyr::rbind.fill(groups, df)
     }
   }
-
+  
   # get unique groups (since represented multiple times for each site looped through, above)
   groups <- unique(groups)
-
+  
   if (dim(groups)[1] > 0) { # if there are groups of nearby sites...
     # create group ID's for easier understanding
-    grp <- data.frame(TADA.SiteGroup = unique(groups$TADA.SiteGroup), TADA.SiteGroupID = paste0("Group_", 1:length(unique(groups$TADA.SiteGroup))))
-    groups <- merge(groups, grp, all.x = TRUE)
-    groups <- unique(groups[, !names(groups) %in% c("TADA.SiteGroup")])
-
+    # grp <- data.frame(TADA.SiteGroup = unique(groups$TADA.SiteGroup), TADA.SiteGroupID = paste0("Group_", 1:length(unique(groups$TADA.SiteGroup))))
+    # groups <- merge(groups, grp, all.x = TRUE)
+    # groups <- unique(groups[, !names(groups) %in% c("TADA.SiteGroup")])
+    
     # find any sites within multiple groups
     summ_sites <- groups %>%
       dplyr::group_by(MonitoringLocationIdentifier) %>%
       dplyr::mutate(GroupCount = 1:length(MonitoringLocationIdentifier))
-
+    
     # pivot wider if a site belongs to multiple groups
     groups_wide <- merge(groups, summ_sites, all.x = TRUE)
-    groups_wide <- tidyr::pivot_wider(groups_wide, id_cols = "MonitoringLocationIdentifier", names_from = "GroupCount", names_prefix = "TADA.SiteGroup", values_from = "TADA.SiteGroupID")
+    groups_wide <- tidyr::pivot_wider(groups_wide, id_cols = "MonitoringLocationIdentifier", names_from = "GroupCount", names_prefix = "TADA.MonitoringLocationIdentifier", values_from = "TADA.MonitoringLocationIdentifier")
     # merge data to site groupings
     .data <- merge(.data, groups_wide, all.x = TRUE)
-
+    
     # concatenate and move site id cols to right place
-    grpcols <- names(.data)[grepl("TADA.SiteGroup", names(.data))]
-
-    .data <- .data %>% tidyr::unite(col = TADA.NearbySiteGroups, dplyr::all_of(grpcols), sep = ", ", na.rm = TRUE)
-    .data$TADA.NearbySiteGroups[.data$TADA.NearbySiteGroups == ""] <- "No nearby sites"
+    grpcols <- names(.data)[grepl("TADA.MonitoringLocationIdentifier", names(.data))]
+    
+    .data <- .data %>% tidyr::unite(col = TADA.MonitoringLocationIdentifier, dplyr::all_of(grpcols), sep = ", ", na.rm = TRUE)
   }
-
-  if (dim(groups)[1] == 0) { # if no groups, give a TADA.NearbySiteGroups column filled with NA
-    .data$TADA.NearbySiteGroups <- "No nearby sites"
+  
+  .data <- .data %>% 
+    dplyr::mutate(TADA.MonitoringLocationIdentifier = ifelse(TADA.MonitoringLocationIdentifier == "", MonitoringLocationIdentifier, TADA.MonitoringLocationIdentifier))   
+  
+  if (dim(groups)[1] == 0) { # #if no groups, give a TADA.MonitoringLocationIdentifier column filled with NA
     print("No nearby sites detected using input buffer distance.")
   }
-
+  
   # order columns
   if ("ResultIdentifier" %in% names(.data)) {
     .data <- TADA_OrderCols(.data)
   }
-
+  
   return(.data)
 }
 
