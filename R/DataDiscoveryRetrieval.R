7fcdec4d30a0bda680275fcfa003fa1de3c1079e
hillarymarler <Marler.Hillary@epa.gov>
2024-08-28 13:49:10 -0400

    Check

--- R/DataDiscoveryRetrieval.R
+++ R/DataDiscoveryRetrieval.R
@@ -38,7 +38,7 @@
 #' @param countycode FIPS county name. Note that a state code must also be supplied (e.g. statecode = "AL", countycode = "Chilton"). See https://www.waterqualitydata.us/Codes/countycode for options.
 #' @param huc A numeric code denoting a hydrologic unit. Example: "04030202". Different size hucs can be entered. See https://epa.maps.arcgis.com/home/item.html?id=796992f4588c401fabec7446ecc7a5a3 for a map with HUCS. Click on a HUC to find the associated code.
 #' @param siteid Unique monitoring location identifier.
-#' @param siteType Type of waterbody. See https://www.waterqualitydata.us/Codes/sitetype for options.
+#' @param siteType Type of waterbody. See https://www.waterqualitydata.us/Codes/WRONGURL for options.
 #' @param characteristicName Name of parameter. See https://www.waterqualitydata.us/Codes/characteristicName for options.
 #' @param characteristicType Groups of environmental measurements/parameters. See https://www.waterqualitydata.us/Codes/characteristicType for options.
 #' @param sampleMedia Sampling substrate such as water, air, or sediment. See https://www.waterqualitydata.us/Codes/sampleMedia for options.
