d358be531be7ae3d5afaa088a16470ae49829708
hillarymarler <Marler.Hillary@epa.gov>
2024-08-29 18:26:44 -0400

    Update test-CriteriaComparison.R

--- tests/testthat/test-CriteriaComparison.R
+++ tests/testthat/test-CriteriaComparison.R
@@ -1,9 +1,11 @@
 test_that("TADA_PairForCriteriaCalc function does not grow dataset", {
+  
   testdat1 <- TADA_RandomTestingData(
-    choose_random_state = FALSE,
+    choose_random_state = TRUE,
     number_of_days = 1,
-    autoclean = 
+    autoclean = TRUE
   )
+  
   testdat2 <- TADA_PairForCriteriaCalc(testdat1)
   expect_true(dim(testdat1)[1] == dim(testdat2)[1])
 })
