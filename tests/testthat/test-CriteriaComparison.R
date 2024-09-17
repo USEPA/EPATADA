01c40fd38fc6d7f2860fb49e7904a8b4165e5f41
Kenny Wong <Wong.Kenny@epa.gov>
2024-09-17 15:09:32 -0400

    Update test-CriteriaComparison.R
    The test_that should re-run the TADA_RandomTestingData pull up to 10 times for testdat1. It will stop and determine if dimensions are the same. if testdat2 is able to run without error, that is if paired parameters in the testdat1 are in its dataframe.

--- tests/testthat/test-CriteriaComparison.R
+++ tests/testthat/test-CriteriaComparison.R
@@ -1,12 +1,32 @@
+
 test_that("TADA_PairForCriteriaCalc function does not grow dataset", {
   
   testdat1 <- TADA_RandomTestingData(
-    choose_random_state = TRUE,
     number_of_days = 1,
+    choose_random_state = TRUE,
     autoclean = TRUE
-  )
-  
-  testdat2 <- TADA_PairForCriteriaCalc(testdat1)
+    )
+    
+    tryCatch({
+        testdat2 <- TADA_PairForCriteriaCalc (testdat1)
+      }
+      ,
+      error = function (testdat1) {
+        for(i in 1:10) {
+          testdat1 <- TADA_RandomTestingData (
+            number_of_days = 1,
+            choose_random_state = TRUE,
+            autoclean = TRUE
+          )
+        
+        if( class( try( testdat2 <- TADA_PairForCriteriaCalc(testdat1))) != "try-error") {
+          testdat2
+          opt <- options(show.error.messages=FALSE)
+          on.exit(options(opt))
+          stop()
+        }
+      }
+    })
+    
   expect_true(dim(testdat1)[1] == dim(testdat2)[1])
 })
-
