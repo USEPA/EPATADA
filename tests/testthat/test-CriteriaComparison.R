5b155a3564fd95bef9faaf9661fd7e8f57bacc81
Kenny Wong <wong.kenny@epa.gov>
2024-10-10 09:55:12 -0400

    Update test-CriteriaComparison.R
    used break to break the loop.
    
    defined testdat as a global variable list that contains the two dataframes in the trycatch error function of testdat1 and testdat2

--- tests/testthat/test-CriteriaComparison.R
+++ tests/testthat/test-CriteriaComparison.R
@@ -1,17 +1,19 @@
 test_that("TADA_PairForCriteriaCalc function does not grow dataset", {
-  testdat1 <- TADA_RandomTestingData(
-    number_of_days = 1,
-    choose_random_state = TRUE,
-    autoclean = TRUE
-  )
-
+  
   # checks to see if testdat1 has any paired parameters needed to run TADA_PairForCriteriaCalc.
   # If it fails to run, an error will result and it will reiterate the testdat1 <- TADA_RandomTestingData() pull.
-  tryCatch(
+  testdat <- tryCatch(
     {
+      testdat1 <- TADA_RandomTestingData(
+        number_of_days = 1,
+        choose_random_state = TRUE,
+        autoclean = TRUE)
       testdat2 <- TADA_PairForCriteriaCalc(testdat1)
+      testdat <- list(testdat1, testdat2)
     },
-    error = function(testdat1) {
+    error = function(err) {
+      testdat1 <- data.frame()
+      testdat2 <- data.frame()
       for (i in 1:10) {
         testdat1 <- TADA_RandomTestingData(
           number_of_days = 1,
@@ -21,14 +23,19 @@ test_that("TADA_PairForCriteriaCalc function does not grow dataset", {
         # Checks if testdat2 will run without error with the new testdat1 data pull.
         # If try() is able to run without error, then the loop will stop.
         if (class(try(testdat2 <- TADA_PairForCriteriaCalc(testdat1))) != "try-error") {
-          testdat2
-          opt <- options(show.error.messages = FALSE)
-          on.exit(options(opt))
-          stop()
+          message("Initial test failed. Retrying. Loop has ran through ",i," time(s)")
+          testdat2 <- TADA_PairForCriteriaCalc(testdat1)
+          testdat <- list(testdat1, testdat2)
+          return(testdat)
+          break
         }
       }
+      testdat <- list(testdat1, testdat2)
+      return(testdat)
     }
   )
+  testdat1 <- testdat[[1]]
+  testdat2 <- testdat[[2]]
 
   expect_true(dim(testdat1)[1] == dim(testdat2)[1])
-})
+  })
