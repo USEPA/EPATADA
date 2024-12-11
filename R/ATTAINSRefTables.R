2cc7e53d47cf669b69ac6ae220f10dd965510fde
hillarymarler <Marler.Hillary@epa.gov>
2024-12-11 09:25:29 -0500

    Create draft crosswalk function

---
+++ R/ATTAINSRefTables.R
@@ -0,0 +1,45 @@
+#' ATTAINS Organization Identifier Reference Table
+#'
+#' Function downloads and returns the newest available crosswalk of state/tribe/territory codes
+#' and their respective organization names and organization identifiers.
+#'
+#' This function caches the table after it has been called once
+#' so subsequent calls will be faster.
+#'
+#' @return Updated sysdata.rda with updated ATTAINSOrgIDsRef object
+#'
+#' @export
+
+TADA_GetATTAINSOrgIDsRef <- function() {
+  # If there is a cached table available return it
+  if (!is.null(ATTAINSOrgIDsRef_Cached)) {
+    return(ATTAINSOrgIDsRef_Cached)
+  }
+  
+  # Try to download up-to-date raw data
+  
+  raw.data <- tryCatch(
+    {
+      # get data from ATTAINS
+    rATTAINS::domain_values(domain_name = "OrgName")
+    },
+    error = function(err) {
+      NULL
+    }
+  )
+  
+  # If the download failed fall back to internal data (and report it)
+  if (is.null(raw.data)) {
+    message("Downloading latest ATTAINS Organization Reference Table failed!")
+    message("Falling back to (possibly outdated) internal file.")
+    return(utils::read.csv(system.file("extdata", "ATTAINSOrgIDsRef.csv", package = "EPATADA")))
+  }
+  
+  ATTAINSOrgIDsRef <- raw.data %>%
+    dplyr::distinct()
+  
+  # Save updated table in cache
+  ATTAINSOrgIDsRef_Cached <- ATTAINSOrgIDsRef
+  
+  ATTAINSOrgIDsRef
+}
---
+++ R/CrosswalksFromATTAINS.R
@@ -0,0 +1,72 @@
+#' Get MonitoringLocationIdentifier and ATTAINS.assessmentunitidentifier Crosswalk
+#'
+#' If the organization has recorded MonitoringLocationIdentifiers associated with their
+#' Assessment Units in ATTAINS, this function can be used to create a crosswalk of known
+#' MonitoringLocationIdentifier and Assessment Units.
+#'
+#' @param org_id The ATTAINS organization identifier must be supplied by the user. A list of
+#' organization identifiers can be found by downloading the ATTAINS Domains Excel file:
+#' https://www.epa.gov/system/files/other-files/2023-09/DOMAINS.xlsx. Organization identifiers
+#' are listed in the "OrgName" tab. The "code" column contains the organization identifiers that
+#' should be used for this param.
+#'
+#'
+#' @return A dataframe with three columns, ATTAINS.assessmentunitidentifier,
+#' MonitoringLocationIdentiifer, and OrganizationIdentifier is returned.
+#'
+#' @export
+#'
+#' @examples
+#' # Alaska example
+#' AK_crosswalk <- TADA_GetAssessmentUnitCrosswalk(org_id = "AKDECWQ")
+#'
+#' \dontrun{
+#'
+#' }
+
+#'
+TADA_GetAssessmentUnitCrosswalk <- function(org_id = NULL) {
+  if (is.null(org_id)) {
+    stop("An organization identifier must be supplied to create a crosswalk.")
+  } 
+  
+  if (!is.null(org_id)) {
+    au_info <- rATTAINS::assessment_units(organization_id = org_id)
+
+    au_crosswalk <- au_info %>%
+      tidyr::unnest(monitoring_stations) %>%
+      dplyr::select(
+        monitoring_location_identifier, monitoring_organization_identifier,
+        assessment_unit_identifier
+      ) %>%
+      dplyr::filter(!is.na(monitoring_location_identifier)) %>%
+      dplyr::distinct() %>%
+      dplyr::rename(
+        ATTAINS.assessmentunitidentifier = assessment_unit_identifier,
+        MonitoringLocationIdentifier = monitoring_location_identifier,
+        OrganizationIdentifier = monitoring_organization_identifier
+      )
+
+    rm(au_info)
+    
+    if (length(au_crosswalk$MonitoringLocationIdentifier > 0)) {
+      print(paste0(
+        "There are ", nrow(au_crosswalk),
+        " MonitoringLocationIdentifiers associated with Assessment Units for ",
+        org_id, " in ATTAINS."
+      ))
+      
+      return(au_crosswalk)
+    }
+
+    if (length(au_crosswalk$MonitoringLocationIdentifier) == 0) {
+      stop(paste0(
+        "No MonitoringLocationIdentifiers were recorded in ATTAINS for ",
+        org_id, " Assessment Units."
+      ))
+    }
+
+   
+  }
+}
+
