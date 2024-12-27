a4ea801f892ca4e7f169a3a3d96044be2a885800
hillarymarler <Marler.Hillary@epa.gov>
2024-12-26 19:43:07 -0500

    Update Utilities.R
    Update global variables

--- R/Utilities.R
+++ R/Utilities.R
@@ -92,7 +92,8 @@ utils::globalVariables(c(
   "ATTAINS.submissionid", "HorizontalCoordinateReferenceSystemDatumName", 
   "NCount", "NHD.catchmentareasqkm", "NHD.comid", "NHD.nhdplusid", "NHD.resolution", 
   "areasqkm", "assessmentUnitIdentifier", "catchmentareasqkm", "comid", 
-  "featureid", "geometry", "nhdplusid", "waterTypeCode"
+  "featureid", "geometry", "nhdplusid", "waterTypeCode", "TADA.SiteGroup", 
+  "TADA.MonitoringLocationIdentifier.New", "TADA.NearbySites.Flag"
 ))
 
 # global variables for tribal feature layers used in TADA_OverviewMap in Utilities.R
