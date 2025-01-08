c2f52365bee533aafd8e7be6f37b47bd47375792
cristinamullin <46969696+cristinamullin@users.noreply.github.com>
2025-01-08 15:47:45 -0500

    fix pH unit harmonization

--- tests/testthat/test-Utilities.R
+++ tests/testthat/test-Utilities.R
@@ -8,28 +8,35 @@ test_that("TADA_AutoClean function does not grow dataset", {
   expect_true(dim(testautoclean1)[1] == dim(testautoclean2)[1])
 })
 
-test_that("TADA_AutoClean function harmonizes pH units", {
+test_that("TADA_AutoClean: pH harmonization works as expected", {
   
-  get_random_pH_data <- TADA_RandomTestingData(
-    choose_random_state = TRUE,
-    number_of_days = 5,
+  # get random pH data
+  random_data <- TADA_RandomTestingData(
+    choose_random_state = FALSE,
+    number_of_days = 1,
     autoclean = FALSE
   )
+  random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
   
-  pHtest <- dplyr::filter(get_random_pH_data, CharacteristicName %in% "pH")
-  
-  verify_random_data <- function() {
-    df <- pHtest()
-    while(nrow(df) < 200) {
-      df <- get_random_pH_data()
-    }
-    return(df)
+  while(nrow(random_pH_data) < 2) {
+    random_data <- TADA_RandomTestingData(
+      choose_random_state = FALSE,
+      number_of_days = 1,
+      autoclean = FALSE
+    )
+    random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
   }
   
-  pHtest2 <- TADA_AutoClean(pHtest)
+  # TADA_AutoClean harmonizes pH, and other mod 1 required functions remove garbage data
+  pHtest <- TADA_AutoClean(random_data)
+  pHtest2 <- TADA_SimpleCensoredMethods(pHtest)
+  pHtest3 <- TADA_AutoFilter(pHtest2)
+  pHtest4 <- TADA_RunKeyFlagFunctions(pHtest3)
+  pHtest5 <- TADA_HarmonizeSynonyms(pHtest4)
   
-  print(unique(pHtest2$TADA.ResultMeasure.MeasureUnitCode))
-  
-  expect_true(unique(pHtest2$TADA.ResultMeasure.MeasureUnitCode) == "STD UNITS")
+  # Is pH data harmonized after above mod 1 functions have run?
+  pHtest6 <- dplyr::filter(pHtest5, CharacteristicName %in% "pH")
+  print(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode))
+  expect_true(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode) == "STD UNITS")
   
 })
