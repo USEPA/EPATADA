d18b0642034a9f31eb399c251109089f8fad363a
cristinamullin <46969696+cristinamullin@users.noreply.github.com>
2025-03-17 16:13:34 -0400

    update example data

--- dev/Maintenance.R
+++ dev/Maintenance.R
@@ -312,88 +312,88 @@ TADA_UpdateExampleData <- function() {
 
 ###########################################################
 
-# # Run devtools check and test
-# devtools::check()
-# devtools::check(manual = TRUE, remote = TRUE, incoming = TRUE) # more robust test for releases (includes broken link check)
-# devtools::test()
+# un devtools check and test
+devtools::check()
+devtools::check(manual = TRUE, remote = TRUE, incoming = TRUE) # more robust test for releases (includes broken link check)
+devtools::test()
 
 ###########################################################
 
-# # spell check
-# library(spelling)
-# spelling::spell_check_package(
-#   pkg = ".",
-#   vignettes = TRUE
-# )
-# # run to update spelling word list
-# spelling::get_wordlist()
-# spelling::update_wordlist()
+# spell check
+library(spelling)
+spelling::spell_check_package(
+  pkg = ".",
+  vignettes = TRUE
+)
+# run to update spelling word list
+spelling::get_wordlist()
+spelling::update_wordlist()
 
 ###########################################################
 
-# # Find Broken Links if test-URLChecker.R fails
-# # Run the code below:
-# # extract urls function
-# extract_urls <- function(text) {
-#   stringr::str_extract_all(text, "http[s]?://[^\\s\\)\\]]+") %>% unlist()
-# }
-# 
-# # clean urls function
-# clean_url <- function(url) {
-#   stringr::str_remove_all(url, "[\\\\.,\\\")]+$|[{}].*") %>%
-#     stringr::str_remove_all("[<>]")
-# }
-# 
-# # create lists of files to check
-# other_files <- c(
-#   system.file("README.md", package = "EPATADA"),
-#   system.file("DESCRIPTION", package = "EPATADA"),
-#   system.file("NAMESPACE", package = "EPATADA")
-# )
-# 
-# vignettes <- list.files(system.file("vignettes", package = "EPATADA"), pattern = ".Rmd", full.names = TRUE)
-# 
-# articles <- list.files(system.file("vignettes/articles", package = "EPATADA"), pattern = ".Rmd", full.names = TRUE)
-# 
-# r_files <- list.files(system.file("R", package = "EPATADA"), pattern = ".R", full.names = TRUE)
-# 
-# # combine file lists
-# files <- append(other_files, vignettes) %>%
-#   append(articles) %>%
-#   append(r_files)
-# 
-# # create list of urls
-# urls <- purrr::map(files, ~ readLines(.x)) %>%
-#   unlist() %>%
-#   extract_urls() %>%
-#   clean_url() %>%
-#   unique() %>%
-#   # problematic URL I can't get a response from using multiple methods (itec) and CRAN because its response is inconsistent, likely due to redirecting to mirrors (HRM 10/28/2024)
-#   setdiff(c(
-#          # url works (HRM 11/7/24), but does not provide a recognizable response code
-#             "https://www.itecmembers.org/attains/",
-#          # if included will get 500 response because this is an incomplete URL
-#          # additional query information is pasted in as part of geospatial functions
-#           "https://attains.epa.gov/attains-public/api/assessmentUnits?assessmentUnitIdentifier=",
-#          # page loads but does not return a response code (NA)
-#          "http://cran.us.r-project.org"
-#           ))
-# 
-# # retrieve http response headers from url list
-# headers <- urls %>%
-#   purrr::map(~ tryCatch(curlGetHeaders(.x), error = function(e) NA))
-# 
-# # extract response code from first line of header response
-# response_code <- sapply(headers, "[[", 1)
-# 
-# # create dataframe of urls and response codes
-# df <- data.frame(urls, response_code)
-# 
-# # filter for any response codes that are not successful or redirect responses
-# df_false <- df %>%
-#   dplyr::filter(!grepl("200", response_code) &
-#                   !grepl("301", response_code) &
-#                   !grepl("302", response_code))
+# Find Broken Links if test-URLChecker.R fails
+# Run the code below:
+# extract urls function
+extract_urls <- function(text) {
+  stringr::str_extract_all(text, "http[s]?://[^\\s\\)\\]]+") %>% unlist()
+}
+
+# clean urls function
+clean_url <- function(url) {
+  stringr::str_remove_all(url, "[\\\\.,\\\")]+$|[{}].*") %>%
+    stringr::str_remove_all("[<>]")
+}
+
+# create lists of files to check
+other_files <- c(
+  system.file("README.md", package = "EPATADA"),
+  system.file("DESCRIPTION", package = "EPATADA"),
+  system.file("NAMESPACE", package = "EPATADA")
+)
+
+vignettes <- list.files(system.file("vignettes", package = "EPATADA"), pattern = ".Rmd", full.names = TRUE)
+
+articles <- list.files(system.file("vignettes/articles", package = "EPATADA"), pattern = ".Rmd", full.names = TRUE)
+
+r_files <- list.files(system.file("R", package = "EPATADA"), pattern = ".R", full.names = TRUE)
+
+# combine file lists
+files <- append(other_files, vignettes) %>%
+  append(articles) %>%
+  append(r_files)
+
+# create list of urls
+urls <- purrr::map(files, ~ readLines(.x)) %>%
+  unlist() %>%
+  extract_urls() %>%
+  clean_url() %>%
+  unique() %>%
+  # problematic URL I can't get a response from using multiple methods (itec) and CRAN because its response is inconsistent, likely due to redirecting to mirrors (HRM 10/28/2024)
+  setdiff(c(
+         # url works (HRM 11/7/24), but does not provide a recognizable response code
+            "https://www.itecmembers.org/attains/",
+         # if included will get 500 response because this is an incomplete URL
+         # additional query information is pasted in as part of geospatial functions
+          "https://attains.epa.gov/attains-public/api/assessmentUnits?assessmentUnitIdentifier=",
+         # page loads but does not return a response code (NA)
+         "http://cran.us.r-project.org"
+          ))
+
+# retrieve http response headers from url list
+headers <- urls %>%
+  purrr::map(~ tryCatch(curlGetHeaders(.x), error = function(e) NA))
+
+# extract response code from first line of header response
+response_code <- sapply(headers, "[[", 1)
+
+# create dataframe of urls and response codes
+df <- data.frame(urls, response_code)
+
+# filter for any response codes that are not successful or redirect responses
+df_false <- df %>%
+  dplyr::filter(!grepl("200", response_code) &
+                  !grepl("301", response_code) &
+                  !grepl("302", response_code))
 
 # Review the output of df_false.
 # More information about http response codes can be found here:
