b1549b1bdf4d52b7629226fa4e6e9d45873b720d
Katie/Ryn Willi (she/her) <49761053+kathryn-willi@users.noreply.github.com>
2025-03-27 18:09:12 -0600

    Update R/ContinuousDataFunctions.R
    Co-authored-by: Matt Brousil <37380883+mbrousil@users.noreply.github.com>

--- R/ContinuousDataFunctions.R
+++ R/ContinuousDataFunctions.R
@@ -44,6 +44,7 @@
 #' }
 #'
 TADA_listNWIS <- function(aoi_sf = "null", states = "null", sites = "null"){
+# Confirm only a single argument has been provided
   if (!sum(purrr::map_lgl(list(aoi_sf, states[1], sites[1]), ~ is.null(.x) || (is.character(.x) && .x == "null"))) %in% c(2, 3)) {
     stop(
       paste0(
