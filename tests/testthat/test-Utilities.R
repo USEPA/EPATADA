7bd083213bec187281112eac52a68a58af9209da
cristinamullin <46969696+cristinamullin@users.noreply.github.com>
2025-05-12 17:09:48 -0400

    update TADAModule3a

--- tests/testthat/test-Utilities.R
+++ tests/testthat/test-Utilities.R
@@ -16,7 +16,7 @@ test_that("TADA_AutoClean function does not grow dataset", {
 #    autoclean = FALSE
 #  )
 #  random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
-#
+# 
 #  while (nrow(random_pH_data) < 2) {
 #    random_data <- TADA_RandomTestingData(
 #      choose_random_state = FALSE,
@@ -25,14 +25,13 @@ test_that("TADA_AutoClean function does not grow dataset", {
 #    )
 #    random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
 # }
-
 #  # TADA_AutoClean harmonizes pH, and other mod 1 required functions remove garbage data
 #  pHtest <- TADA_AutoClean(random_data)
 #  pHtest2 <- TADA_SimpleCensoredMethods(pHtest)
 #  pHtest3 <- TADA_AutoFilter(pHtest2)
 #  pHtest4 <- TADA_RunKeyFlagFunctions(pHtest3)
 #  pHtest5 <- TADA_HarmonizeSynonyms(pHtest4)
-#
+# 
 #  # Is pH data harmonized after above mod 1 functions have run?
 #  pHtest6 <- dplyr::filter(pHtest5, CharacteristicName %in% "pH")
 #  print(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode))
