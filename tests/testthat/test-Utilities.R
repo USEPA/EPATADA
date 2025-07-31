98eebb25570392ac6f85531dc1f8929924c35b3d
Mullin <Mullin.Cristina@epa.gov>
2025-07-31 19:44:56 -0400

    add tests

--- tests/testthat/test-Utilities.R
+++ tests/testthat/test-Utilities.R
@@ -8,32 +8,68 @@ test_that("TADA_AutoClean function does not grow dataset", {
   expect_true(dim(testautoclean1)[1] == dim(testautoclean2)[1])
 })
 
-# test_that("TADA_AutoClean: pH harmonization works as expected", {
-#  # get random pH data
-#  random_data <- TADA_RandomTestingData(
-#    choose_random_state = FALSE,
-#    number_of_days = 1,
-#    autoclean = FALSE
-#  )
-#  random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
-#
-#  while (nrow(random_pH_data) < 2) {
-#    random_data <- TADA_RandomTestingData(
-#      choose_random_state = FALSE,
-#      number_of_days = 1,
-#      autoclean = FALSE
-#    )
-#    random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
-# }
-#  # TADA_AutoClean harmonizes pH, and other mod 1 required functions remove garbage data
-#  pHtest <- TADA_AutoClean(random_data)
-#  pHtest2 <- TADA_SimpleCensoredMethods(pHtest)
-#  pHtest3 <- TADA_ConvertSpecialChars(pHtest2, col = "TADA.ResultMeasureValue", clean = TRUE)
-#  pHtest4 <- TADA_RunKeyFlagFunctions(pHtest3)
-#  pHtest5 <- TADA_HarmonizeSynonyms(pHtest4)
-#
-#  # Is pH data harmonized after above mod 1 functions have run?
-#  pHtest6 <- dplyr::filter(pHtest5, CharacteristicName %in% "pH")
-#  print(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode))
-#  expect_true(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode) == "STD UNITS")
-# })
+test_that("TADA_AutoClean: pH harmonization works as expected", {
+ # get random pH data
+ random_data <- TADA_RandomTestingData(
+   choose_random_state = FALSE,
+   number_of_days = 1,
+   autoclean = FALSE
+ )
+ random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
+
+ while (nrow(random_pH_data) < 2) {
+   random_data <- TADA_RandomTestingData(
+     choose_random_state = FALSE,
+     number_of_days = 1,
+     autoclean = FALSE
+   )
+   random_pH_data <- dplyr::filter(random_data, CharacteristicName %in% "pH")
+}
+ # TADA_AutoClean harmonizes pH, and other mod 1 required functions remove nonsensical data
+ pHtest <- TADA_AutoClean(random_data)
+ pHtest2 <- TADA_SimpleCensoredMethods(pHtest)
+ pHtest3 <- TADA_ConvertSpecialChars(pHtest2, col = "TADA.ResultMeasureValue", clean = TRUE)
+ pHtest4 <- TADA_RunKeyFlagFunctions(pHtest3)
+ pHtest5 <- TADA_HarmonizeSynonyms(pHtest4)
+
+ # Is pH data harmonized after above mod 1 functions have run?
+ pHtest6 <- dplyr::filter(pHtest5, CharacteristicName %in% "pH")
+ print(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode))
+ expect_true(unique(pHtest6$TADA.ResultMeasure.MeasureUnitCode) == "NONE")
+})
+
+test_that("Column names do not contain the pattern 'TADA.TADA.'", {
+  test_TADA.TADA. <-
+    TADA_ConvertSpecialChars(
+      Data_Nutrients_UT,
+      "TADA.DetectionQuantitationLimitMeasure.MeasureValue"
+    )
+  # Create a logical vector indicating which columns contain the pattern
+  pattern_found <- grepl("TADA.TADA.", colnames(test_TADA.TADA.))
+  
+  # Test should pass if none of the columns contain the pattern
+  expect_false(any(pattern_found), info = "Some column names contain the pattern 'TADA.TADA.'")
+})
+
+test_that("Column names do not contain the pattern 'TADA.TADA.'", {
+  test_TADA.TADA. <-
+    TADA_ConvertSpecialChars(
+      Data_Nutrients_UT,
+      "TADA.ResultMeasureValue"
+    )
+  # Create a logical vector indicating which columns contain the pattern
+  pattern_found <- grepl("TADA.TADA.", colnames(test_TADA.TADA.))
+  
+  # Test should pass if none of the columns contain the pattern
+  expect_false(any(pattern_found), info = "Some column names contain the pattern 'TADA.TADA.'")
+})
+
+test_that("Column names do not contain the pattern 'TADA.TADA.'", {
+  test_TADA.TADA. <- TADA_AutoClean(Data_R5_TADAPackageDemo)
+  # Create a logical vector indicating which columns contain the pattern
+  pattern_found <- grepl("TADA.TADA.", colnames(test_TADA.TADA.))
+  
+  # Test should pass if none of the columns contain the pattern
+  expect_false(any(pattern_found), info = "Some column names contain the pattern 'TADA.TADA.'")
+})
+
