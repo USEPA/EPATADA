4ab6f4e61e29438285501191160aedd5619e0dc6
Mullin <Mullin.Cristina@epa.gov>
2025-09-16 16:01:57 -0400

    update checkColNames
    typos

--- R/Utilities.R
+++ R/Utilities.R
@@ -1631,10 +1631,10 @@ TADA_RenametoLegacy <- function(.data) {
 #' checkColNames
 #'
 #' This function checks column names using partial string matches. It is designed
-#' to faciliate the use of user-supplied refs with differently prefixed columns
+#' to facilitate the use of user-supplied refs with differently prefixed columns
 #' in Module 2 and 3 functions.
 #'
-#' @param .data A user-supplied ref data farme containing AssessmentUnitIdentifier,
+#' @param .data A user-supplied ref data frame containing AssessmentUnitIdentifier,
 #' MonitoringLocationIdentifier, and WaterType columns. It is permitted (but not
 #' required) for these columns to use ATTAINS, TADA or other prefixes.
 #'
