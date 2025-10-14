d40bcd21bd43e3a0e06d1fbce0e5ed6b44a18938
Mullin <Mullin.Cristina@epa.gov>
2025-10-14 12:52:43 -0400

    update TADA_RandomTesting and TADA_ConvertDepthUnits test
    addresses check issue, removes duplicate TADA_RandomTesting functions

--- tests/testthat/test-UnitConversions.R
+++ tests/testthat/test-UnitConversions.R
@@ -100,10 +100,16 @@ test_that("TADA_ConvertDepthUnits converts meters to m", {
   expect_false("meters" %in% check_depth_meters$TADA.ActivityDepthHeightMeasure.MeasureUnitCode)
 })
 
-# check that TADA_CreateUnitRef contains a row for each TADA.CharacteristicName,
+# Check that TADA_CreateUnitRef contains a row for each TADA.CharacteristicName,
 # and ResultMeasure.MeasureUnitCode
 test_that("TADA_CreateUnitRef output contains a row for each TADA.CharacteristicName, TADA.ResultMeasure.MeasureUnitCode, and ResultMeasure.MeasureUnitCode.", {
-  testdat <- TADA_RandomTestingData(number_of_days = 3, choose_random_state = TRUE)
+  testdat <- TADA_RandomTestingData(number_of_days = 2, choose_random_state = TRUE)
+  
+  # Skip the test if the data retrieval results in an empty data frame
+  if (nrow(testdat) == 0) {
+    skip("Data retrieval failed after multiple attempts, skipping the test.")
+  }
+  
   unit.ref <- TADA_CreateUnitRef(testdat)
   unit.ref <- unit.ref %>%
     dplyr::select(
@@ -111,8 +117,7 @@ test_that("TADA_CreateUnitRef output contains a row for each TADA.Characteristic
       ResultMeasure.MeasureUnitCode
     ) %>%
     dplyr::distinct()
-
-
+  
   unit.combs <- TADA_UniqueCharUnitSpeciation(testdat)
   unit.combs <- unit.combs %>%
     dplyr::select(
@@ -120,7 +125,7 @@ test_that("TADA_CreateUnitRef output contains a row for each TADA.Characteristic
       ResultMeasure.MeasureUnitCode
     ) %>%
     dplyr::distinct()
-
+  
   compare <- unit.ref %>%
     dplyr::anti_join(unit.combs)
   expect_true(nrow(compare) == 0)
