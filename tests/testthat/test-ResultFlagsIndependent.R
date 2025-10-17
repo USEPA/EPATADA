ecb4277b3e75f246723d9cfe077428c41affdf96
Mullin <Mullin.Cristina@epa.gov>
2025-10-17 14:39:12 -0400

    update range flag function logic
    consider ranges suspect if units are suspect, update continuous data function notes, update example data used for flag functions, update example data and refs

--- tests/testthat/test-ResultFlagsIndependent.R
+++ tests/testthat/test-ResultFlagsIndependent.R
@@ -146,13 +146,9 @@ test_that("range flag functions work", {
 
   expect_no_error(TADA_FlagAboveThreshold(upper))
   expect_no_warning(TADA_FlagAboveThreshold(upper))
-  expect_no_message(TADA_FlagAboveThreshold(upper))
-  expect_no_condition(TADA_FlagAboveThreshold(upper))
 
   expect_no_error(TADA_FlagBelowThreshold(upper))
   expect_no_warning(TADA_FlagBelowThreshold(upper))
-  expect_no_message(TADA_FlagBelowThreshold(upper))
-  expect_no_condition(TADA_FlagBelowThreshold(upper))
 })
 
 
