3f5c267357a19102b0e533ec4bb542ced6eb0cf8
Kenny Wong <Wong.Kenny@epa.gov>
2025-12-29 16:28:14 -0500

    epa304 to run when it is the only arg input
    additional edits to mod 3 vignettes

--- R/CriteriaMethods.R
+++ R/CriteriaMethods.R
@@ -200,12 +200,14 @@ TADA_DefineCriteriaMethodology <- function(
   # Return an empty data frame with column names only if a user does not define any arg inputs.
   if (
     missing(.data) &&
-      missing(org_id) &&
-      missing(MLSummaryRef) &&
-      missing(criteriaMethods) &&
-      missing(AUMLRef) &&
-      missing(AU_UsesRef)
+    missing(MLSummaryRef) &&
+    missing(criteriaMethods) &&
+    missing(AUMLRef) &&
+    missing(AU_UsesRef)
   ) {
+    # if (!"USEPA" %in% org_id) {
+    #   stop("org_id can only equal NULL or 'USEPA' if all other argument inputs are left blank.")
+    # }
     message(
       "All arguments are blank, returning an empty dataframe with column names only."
     )
@@ -774,55 +776,6 @@ TADA_DefineCriteriaMethodology <- function(
       )
     }
 
-    # User wants to populate the Criteria table using the EPA304a standards
-    # joins the epa304a standards to the current Criteria Table.
-    if ("USEPA" %in% org_id) {
-      print(paste0(
-        "USEPA was included in your 'org_id': Including EPA304a recommended standards by each unique TADA.CharacteristicName if one is found."
-      ))
-
-      epa304a <- utils::read.csv(system.file(
-        "extdata",
-        "EPA304a_criteria_table.csv",
-        package = "EPATADA"
-      ))
-
-      if (displayUniqueId == TRUE) {
-        uniqueID <- unique(.data[, c(
-          "TADA.ComparableDataIdentifier",
-          "TADA.CharacteristicName"
-        )])
-        epa304a <- epa304a |>
-          dplyr::select(-TADA.ComparableDataIdentifier) |>
-          dplyr::left_join(uniqueID)
-      }
-
-      # read in ref csv
-      coltype.ref <- utils::read.csv(system.file(
-        "extdata",
-        "TADAColTypeRef.csv",
-        package = "EPATADA"
-      ))
-
-      epa304a <- suppressWarnings(correctColType(epa304a)) |>
-        dplyr::select(names(epa304a)[
-          names(epa304a) %in% coltype.ref$column_name
-        ]) |>
-        dplyr::mutate(ATTAINS.ParameterName = toupper(ATTAINS.ParameterName)) |>
-        dplyr::filter(
-          TADA.CharacteristicName %in%
-            DefineCriteriaMethodology$TADA.CharacteristicName
-        )
-
-      DefineCriteriaMethodology <- DefineCriteriaMethodology |>
-        dplyr::filter(
-          !(ATTAINS.OrganizationIdentifier == "USEPA" &
-            TADA.CharacteristicName %in% epa304a$TADA.CharacteristicName)
-        ) |> # filters out the blank EPA304a criteria table but keep any unique
-        # TADA Characteristic not defined from the epa304a criteria table.
-        plyr::rbind.fill(epa304a)
-    }
-
     # Display all unique TADA.ComparableDataIdentifier in the Criteria Methods list or not.
     # Helps a user identifies all WQP data if they do not fill out the reference tables when TRUE
     # FALSE is recommended if a user has gone through a step by step review process to
@@ -844,6 +797,68 @@ TADA_DefineCriteriaMethodology <- function(
         dplyr::distinct()
     }
   }
+  
+  # User wants to populate the Criteria table using the EPA304a standards
+  # joins the epa304a standards to the current Criteria Table.
+  if ("USEPA" %in% org_id) {
+    print(paste0(
+      "USEPA was included in your 'org_id': Including EPA304a recommended standards by each unique TADA.CharacteristicName if one is found."
+    ))
+    
+    epa304a <- utils::read.csv(system.file(
+      "extdata",
+      "EPA304a_criteria_table.csv",
+      package = "EPATADA"
+    ))
+    
+    if (displayUniqueId == TRUE) {
+      uniqueID <- unique(.data[, c(
+        "TADA.ComparableDataIdentifier",
+        "TADA.CharacteristicName"
+      )])
+      epa304a <- epa304a |>
+        dplyr::select(-TADA.ComparableDataIdentifier) |>
+        dplyr::left_join(uniqueID)
+    }
+    
+    # read in ref csv
+    coltype.ref <- utils::read.csv(system.file(
+      "extdata",
+      "TADAColTypeRef.csv",
+      package = "EPATADA"
+    ))
+    
+    if (missing(.data)) {
+      epa304a <- suppressWarnings(correctColType(epa304a)) |>
+        dplyr::select(names(epa304a)[
+          names(epa304a) %in% coltype.ref$column_name
+        ]) |>
+        dplyr::mutate(ATTAINS.ParameterName = toupper(ATTAINS.ParameterName))
+    }
+    
+    if (!missing(.data)) {
+      epa304a <- suppressWarnings(correctColType(epa304a)) |>
+        dplyr::select(names(epa304a)[
+          names(epa304a) %in% coltype.ref$column_name
+        ]) |>
+        dplyr::mutate(ATTAINS.ParameterName = toupper(ATTAINS.ParameterName)) |>
+        dplyr::filter(
+          TADA.CharacteristicName %in%
+            DefineCriteriaMethodology$TADA.CharacteristicName
+        )
+    }
+
+    DefineCriteriaMethodology <- DefineCriteriaMethodology |>
+      # filters out the blank EPA304a criteria table but keep any unique
+      # TADA Characteristic not defined from the epa304a criteria table.
+      dplyr::filter(
+        !(ATTAINS.OrganizationIdentifier == "USEPA" &
+            TADA.CharacteristicName %in% epa304a$TADA.CharacteristicName)
+      ) |>
+      plyr::rbind.fill(epa304a) |>
+      dplyr::arrange(ATTAINS.OrganizationIdentifier != "USEPA")
+  }
+  
   # Generates the excel function (HIGHLY Recommended for users to export)
   if (excel == TRUE) {
     # Excel ref files to be stored in the Downloads folder location.
