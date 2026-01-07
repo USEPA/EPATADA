4d814ade3590f6274c18d0afb8205f8e1135ad71
Kenny Wong <wong.kenny@epa.gov>
2026-01-07 12:51:24 -0500

    Update R/CriteriaMethods.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/CriteriaMethods.R
+++ R/CriteriaMethods.R
@@ -399,7 +399,6 @@ TADA_DefineCriteriaMethodology <- function(
         dplyr::filter(!is.na(ATTAINS.OrganizationIdentifier))
 
       MLSummaryRef <- correctColType(MLSummaryRef)
-      
       # Will include all unique TADA Char/ComparableDataIdentifier to be shown in the criteria table
       MLSummaryRef <- TADA_param |>
         dplyr::full_join(MLSummaryRef, by = names(TADA_param))
