1468e8f4670d25381abceddab2589db0082fa179
Mullin <Mullin.Cristina@epa.gov>
2026-01-08 16:32:52 -0500

    Update requiredMaintenance.R

--- dev/requiredMaintenance.R
+++ dev/requiredMaintenance.R
@@ -13,17 +13,15 @@ spelling::get_wordlist()
 
 ###########################################################
 
-library(styler)
-
-# Recommended styler setup to approximate air.toml
-
-style_pkg(
-  transformers = tidyverse_style(
-    scope = I(c("tokens", "indention")), # let air handle the rest
-    indent_by = 2,
-    strict = FALSE # turn off aggressive alignment
-  )
-)
+# library(styler)
+# Recommended styler setup to approximate air.toml, some edtis still contradict eachother. Do not run.
+# style_pkg(
+#   transformers = tidyverse_style(
+#     scope = I(c("tokens", "indention")), # let air handle the rest
+#     indent_by = 2,
+#     strict = FALSE # turn off aggressive alignment
+#   )
+# )
 
 ###########################################################
 
