7ed49ef4af02bdda9430384af643b31e72423ce9
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-01-08 12:55:49 -0500

    Update GeospatialUtilities.R

--- R/GeospatialUtilities.R
+++ R/GeospatialUtilities.R
@@ -886,10 +886,15 @@ createTADABasemap <- function(.data) {
 #' createBBox
 #'
 #' Internal function to create bounding box for maps based on TADA.LatitudeMeasure
-#' and TADA.LongitudeMeasure columns.
+#' and TADA.LongitudeMeasure columns from a TADA df or ATTAINS geometry.
 #'
 #' @param .data Data frame. Must contain the columns TADA.LatitudeMeasure and
 #' TADA.Longitude measure to set the extent of the map.
+#'
+#' @attains_geo Boolean argument. When attains_geo = TRUE, the function must reference
+#' the geometry column of .data. When attains_geo = FALSE, the function references
+#' the TADA.LatitudeMeasure and TADA.LongitudeMeasure to find the bounding box.
+#' Default is attains_geo = FALSE.
 #'
 #' @param as_vector Boolean argument. When as_vector = TRUE, the bounding box values
 #' are returned as vector. When as_vector = FALSE, values are returned as a bounding
@@ -897,7 +902,9 @@ createTADABasemap <- function(.data) {
 #'
 #' @return A bounding box for use in leaflet mapping functions.
 # Create bounding box
-createBBox <- function(.data, as_vector = TRUE) {
+createBBox <- function(.data, as_vector = TRUE, attains_geo = FALSE) {
+
+  if(attains_geo == FALSE) {
   # create bounding box
   bbox <- sf::st_bbox(
     c(
@@ -908,6 +915,11 @@ createBBox <- function(.data, as_vector = TRUE) {
     ),
     crs = sf::st_crs(.data)
   )
+  }
+
+  if(attains_geo == TRUE) {
+    bbox <- sf:st_bbox(.data, crs = sf::st_crs(.data))
+  }
 
   # return as bounding box
   if (as_vector == FALSE) {
