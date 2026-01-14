20ac8441d45a38d57fd1c0ffd9c26000d7dddd5f
Kenny Wong <Wong.Kenny@epa.gov>
2026-01-14 10:01:15 -0500

    TADA_UsesAliasForReview documentation update

--- R/ATTAINSRefTables.R
+++ R/ATTAINSRefTables.R
@@ -479,16 +479,14 @@ TADA_AdditionalCharAliasForReview <- function(
 #' context2 field from the ATTAINS use_name domain, which acts as a uses category,
 #' with the Human Health and Aquatic Life column indicators from the CST.
 #'
-#' Next, this function compares the ATTAINS.UseName and CST uses by extracting 
-#' each individual words from each use domain string and looks at the percentage
-#'  of words that are a match between each ATTAINS uses and CST uses (and
-#' within ATTAINS and CST, and CST and WQX by CAS) as an alternative
-#' way of finding additional aliases. It is recommended for the TADA
-#' team to review this table and decide whether these aliases are
-#' accurate. Many-to-many matches will occur as they will need a thorough review.
-#' Users should be aware that a CST uses may get duplicated
-#' for each ATTAINS.UseName. It is up to the user to decide CST use should be 
-#' appropriately matched to an ATTAINS.UseName.
+#' Next, this function then compares ATTAINS.UseName and CST uses by extracting
+#' individual words from each use domain string and calculating the percentage
+#' of words that match between each ATTAINS use and CST use. Users are advised
+#' to review this uses alias table and adjust their tolerance levels as desired
+#' to determine the accuracy of the crosswalk. Many-to-many matches may occur
+#' and will require thorough review. Users should be aware that a CST use may
+#' be duplicated for each ATTAINS.UseName. It is the user's responsibility
+#' to ensure that CST uses are appropriately matched to ATTAINS.UseName.
 #'
 #' Note for Development: We should keep a reference file to indicate
 #' which rows have already been reviewed during this process.In addition,
