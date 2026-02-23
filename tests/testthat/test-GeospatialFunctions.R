c51a4b4c7f2e53616a07ed025cf8684fec4e2e03
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-02-23 13:46:46 -0500

    add api key param

--- tests/testthat/test-GeospatialFunctions.R
+++ tests/testthat/test-GeospatialFunctions.R
@@ -247,7 +247,7 @@ testthat::test_that("Get ATTAINS by Assessment Unit ID", {
     )
   )
   # Check .data was updated by adding 83 cols (161+83=244)
-  expect_equal(ncol(actual_default$TADA_with_ATTAINS), 245)
+  expect_equal(ncol(actual_default$TADA_with_ATTAINS), 244)
   # Check results based on number of rows
   expected_rows <- c(0, 5, 1)
   expect_equal(nrow(actual_default$ATTAINS_points), expected_rows[1])
