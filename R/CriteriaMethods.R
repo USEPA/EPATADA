d230f2853b6ca086da401e208d6f275027151a07
Hannah <ainsleyhf@gmail.com>
2026-05-21 14:37:22 -0400

    issue #937
    added if statement to create a list object in environment when excel = F

--- R/CriteriaMethods.R
+++ R/CriteriaMethods.R
@@ -2149,6 +2149,35 @@ TADA_DefineCriteriaMethodology <- function(
   DefineCriteriaMethodology <- suppressWarnings(TADA_CorrectColType(
     DefineCriteriaMethodology
   ))
+  
+  # When excel = FALSE, return all supporting tables as a named list
+  if (isFALSE(excel)) {
+    tmp_xlsx <- tempfile(fileext = ".xlsx")
+    on.exit(unlink(tmp_xlsx), add = TRUE)
+    
+    .TADA_CriteriaDataDictionary(tmp_xlsx)
+    
+    DataDictionary <- openxlsx::read.xlsx(
+      tmp_xlsx,
+      sheet = "DataDictionary",
+      startRow = 2
+    )
+    
+    AllowableValues <- openxlsx::read.xlsx(
+      tmp_xlsx,
+      sheet = "AllowableValues",
+      startRow = 2
+    )
+    
+    out <- list(
+      DefineCriteriaMethodology = DefineCriteriaMethodology,
+      DataDictionary = DataDictionary,
+      AllowableValues = AllowableValues
+    )
+    
+    return(out)
+  }
+  
   return(DefineCriteriaMethodology)
 }
 
@@ -3273,3 +3302,4 @@ TADA_DefineCriteriaMethodology <- function(
   # Save the workbook to an Excel file
   openxlsx::saveWorkbook(wb, downloads_path, overwrite = TRUE)
 }
+
