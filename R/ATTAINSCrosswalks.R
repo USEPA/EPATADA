ae0ff66876149373ea800a786e6377ffa6e961ab
Kenny Wong <Wong.Kenny@epa.gov>
2026-07-06 13:32:28 -0400

    Update ATTAINSCrosswalks.R

--- R/ATTAINSCrosswalks.R
+++ R/ATTAINSCrosswalks.R
@@ -2306,11 +2306,12 @@ TADA_UsesForAnalysis <- function(
       )
 
     if (auto_assign == TRUE) {
-      message(
+      message(paste(
         "TADA_UsesForAnalysis: auto_assign == TRUE was selected, ",
         "    assigning all unique ATTAINS.UseName, by ATTAINS.OrganizationIdentifier, to any ATTAINS.ParameterName that an ",
-        "    organization have not done assessments for in prior ATTAINS cycle. Please review carefully and Exclude rows as needed."
-      )
+        "    organization have not done assessments for in prior ATTAINS cycle. Please review carefully and Exclude rows as needed.",
+        sep = "\r\n"
+      ))
 
       # load ATTAINSParamUseOrgRef
       load(system.file(
