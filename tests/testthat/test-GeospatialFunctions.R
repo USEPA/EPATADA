5dd5bd1ece6e84211b771335b3461d7902b5dc28
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-07-09 11:20:37 -0400

    Update test-GeospatialFunctions.R

--- tests/testthat/test-GeospatialFunctions.R
+++ tests/testthat/test-GeospatialFunctions.R
@@ -2,7 +2,7 @@
 # Tests for the functions in GeoSpatialFunctions.R using sample data
 
 TADA_dataframe <- Data_HUC8_02070004_Mod1Output |>
-  dplyr::filter(TADA.CharacteristicName == "PH")
+  dplyr::filter(TADA.CharacteristicName == "NITRATE")
 
 TADA_spatial <- TADA_MakeSpatial(TADA_dataframe)
 
