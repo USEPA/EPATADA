a92d8fe9a095be1c5daf82079efe9dd06e2b8ad1
Mullin <Mullin.Cristina@epa.gov>
2026-08-07 17:17:35 -0400

    Update test-ResultFlagsDependent.R

--- tests/testthat/test-ResultFlagsDependent.R
+++ tests/testthat/test-ResultFlagsDependent.R
@@ -1,31 +1,37 @@
 test_that("No NAs in dependent flag columns", {
   testthat::skip_on_cran()
   testthat::skip_if_offline("www.waterqualitydata.us")
+  
   today <- Sys.Date()
   twoago <- as.character(today - 2 * 365)
-  testdat <- TADA_DataRetrieval(
-    statecode = "UT",
-    startDate = twoago,
-    characteristicName = c("Nitrate", "Copper"),
-    sampleMedia = "Water",
-    ask = FALSE
+  
+  testdat <- tryCatch(
+    TADA_DataRetrieval(
+      statecode = "UT",
+      startDate = twoago,
+      characteristicName = c("Nitrate", "Copper"),
+      sampleMedia = "Water",
+      ask = FALSE
+    ),
+    error = function(e) testthat::skip(paste("Data retrieval failed:", conditionMessage(e)))
   )
+  
   testdat <- TADA_ConvertResultUnits(testdat, transform = TRUE)
-
+  
   testdat <- suppressWarnings(TADA_FlagFraction(
     testdat,
     clean = FALSE,
     flaggedonly = FALSE
   ))
   expect_false(any(is.na(testdat$TADA.SampleFraction.Flag)))
-
+  
   testdat <- suppressWarnings(TADA_FlagSpeciation(
     testdat,
     clean = "none",
     flaggedonly = FALSE
   ))
   expect_false(any(is.na(testdat$TADA.MethodSpeciation.Flag)))
-
+  
   testdat <- suppressWarnings(TADA_FlagResultUnit(
     testdat,
     clean = "none",
